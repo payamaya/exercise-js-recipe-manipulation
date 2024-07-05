b9b92e86dea5d5b424f29b5f666555e5c3adb11c
payamaya <paulyashou81@gmail.com>
2024-07-05 15:41:00 +0200

    changing the image src

--- wrong.js
+++ wrong.js
@@ -3,3 +3,17 @@ const header2 = (document.querySelector('header').style.justifyContent =
   'flex-start')
 const headerBorder = (document.querySelector('header').style.borderBottom =
   '1px solid green')
+
+// 4)
+const changeRecipeName = document.querySelector('h1')
+changeRecipeName.innerText = 'Frozen Cheeseake'
+// 5)The clock icon beneath the recipe name has disappeared and been replaced by a text instead. This can be fixed by adding a class to that element.
+const addIcon = document.querySelector('.time-container span')
+addIcon.classList.add('material-icons')
+
+// 6)The estimated time of the recipe is also incorrect. Change it to the correct time estimation.
+const estimateTime = document.querySelector('.time')
+estimateTime.innerText = '60 + min'
+// 7)The src path to the image is wrong, or atleast it's showing the wrong image. Change it to the correct one. The available images can be found in the assets folder.
+const imgElement = document.querySelector('.image-container img') // Select the <img> element
+imgElement.src = '/assets/frozen-cheesecake-slice.jpg'
