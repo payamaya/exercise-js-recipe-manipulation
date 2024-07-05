a5b28a246f96dc6c4b780778cfbedefb47965584
payamaya <paulyashou81@gmail.com>
2024-07-05 15:29:29 +0200

    change logo color, align element and change borderBottomColor

--- wrong.js
+++ wrong.js
@@ -1,2 +1,5 @@
-const header = document.querySelector('header')
-console.log(header)
+const header = (document.querySelector('.logo-text').style.color = 'black')
+const header2 = (document.querySelector('header').style.justifyContent =
+  'flex-start')
+const headerBorder = (document.querySelector('header').style.borderBottom =
+  '1px solid green')
